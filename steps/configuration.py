213b86d62322aa70d2f53e9acabee32f9b429d07
Claudio Klingler <ck@realtime-projects.com>
2024-04-25 12:22:27 +0200

    Add steps for configuration updates.

---
+++ steps/configuration.py
@@ -0,0 +1,32 @@
+import yaml
+import json
+import shutil
+
+from radish import given, before
+
+from acre.lib import log
+
+
+@before.all
+def load_config(features, marker):
+    shutil.copy("mkdocs.yml.in", "test-doc/mkdocs.yml")
+
+
+@given('I set the configuration {configitem:QuotedString} to {value:QuotedString}')
+def i_see_term(step, configitem, value):
+    with open("test-doc/mkdocs.yml", "r") as file:
+        cfg = yaml.safe_load(file)
+    tree = configitem.split(".")
+
+    for entry in cfg['plugins']:
+        if "ezglossary" in entry:
+            selector = entry['ezglossary']
+
+    for element in tree[:-1]:
+        log.trace(f"selector is: {json.dumps(selector, indent=4)}")
+    if value in ["true", "false"]:
+        value = bool(value)
+    selector[tree[-1]] = value
+
+    with open("test-doc/mkdocs.yml", "w") as file:
+        cfg = yaml.dump(cfg, file)
