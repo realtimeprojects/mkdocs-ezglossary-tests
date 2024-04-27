09f8962257a191a574351ab3af3ad98bbd4ccdb9
Claudio Klingler <ck@realtime-projects.com>
2024-04-27 16:06:40 +0200

    use playwright controls.

--- steps/definition.py
+++ steps/definition.py
@@ -1,32 +1,22 @@
-from playwright.sync_api import TimeoutError as pwTimeoutError
-
 from radish import then, world
 from yaxp import xpath
 import html
 
 from acre.lib import log
+from acre import controls
 
 from mkdocs_ezglossary_plugin import glossary
 
 
-class Definition:
-    def __init__(self, section, term):
-        self._section = section
+class TermDefinition(controls.Control):
+    def __init__(self, term, section=None, parent=None):
+        super().__init__(parent)
+        self._section = section if section else "_"
         self._term = term
 
-    def wait_for(self):
-        return self._locator.wait_for()
-
-    def exists(self, timeout=1000):
-        try:
-            self._locator.wait_for(timeout=timeout)
-        except pwTimeoutError:
-            return False
-        return True
-
     @property
-    def _locator(self):
-        return world.page.locator(str(self._xpath))
+    def locator(self):
+        return self.parent.locator(str(self._xpath))
 
     @property
     def _xpath(self):
@@ -41,12 +31,12 @@ class Definition:
 @then('I see the term definition {term:QuotedString} in section {section:QuotedString}')
 def i_see_term(step, section, term):
     log.trace(f"Checking definition for {section}:{term}")
-    Definition(section, term).wait_for()
+    TermDefinition(term, section).wait_for()
 
 
 @then('I see no term definition {term:QuotedString} in section {section:QuotedString}')
 def i_see_no_term(step, section, term):
-    assert not Definition(section, term).exists(), f"{section}:{term} definition found"
+    assert not TermDefinition(term, section).exists(), f"{section}:{term} definition found"
 
 
 @then('the term definition {term:QuotedString} has description {description:QuotedString}')
@@ -54,9 +44,9 @@ def term_has_description(step, term, description):
     if ":" in term:
         (section, term) = term.split(":")
     else:
-        (section, term) = ("_", term)
+        (section, term) = (None, term)
 
     log.trace(f"Checking definition for {section}:{term}")
-    defn = Definition(section, term)
+    defn = TermDefinition(term, section)
     log.debug(f"--- {defn.description.text_content()}")
     defn.description.filter(has_text=description).wait_for()
--- steps/links.py
+++ steps/links.py
@@ -1,55 +1,25 @@
-from playwright.sync_api import TimeoutError as pwTimeoutError
-
-from radish import when, then, world
-from yaxp import xpath
+from radish import when, then
 import html
 
+from acre import controls
 
-class Link:
-    def __init__(self, section, term, title=None):
-        self._section = section
-        self._term = term
-        self._title = title
 
-    def exists(self, timeout=1000):
-        try:
-            self._locator.wait_for(timeout=timeout)
-        except pwTimeoutError:
-            return False
-        return True
-
-    def wait_for(self):
-        return self._locator.wait_for()
-
-    def click(self):
-        self._locator.click()
-
-    @property
-    def _locator(self):
-        return world.page.locator(str(self._xpath))
-
-    @property
-    def _xpath(self):
-        xp = xpath.article.a(_=f"*{html.unescape(self._term)}", name=f"{self._section}:{self._term}")
-        if self._title:
-            xp = xp(title=self._title)
-        return xp
-
-    @property
-    def description(self):
-        return world.page.locator(str(xpath.dd.following(self._xpath))).first
+class TermLink(controls.Link):
+    def __init__(self, term, section=None, title=None):
+        section = section if section else "_"
+        super().__init__(text=html.unescape(term), name=f"{section}:{term}", title=title)
 
 
 @then('I see a link to {term:QuotedString} in section {section:QuotedString}')
 def i_see_link(step, section, term):
-    Link(section, term).wait_for()
+    TermLink(term, section).wait_for()
 
 
 @when('I click the link for {term:QuotedString} in section {section:QuotedString}')
 def i_click_link(step, section, term):
-    Link(section, term).click()
+    TermLink(term, section).click()
 
 
 @then("the link to {term:QuotedString} in section {section:QuotedString} has title {title:QuotedString}")
 def link_has_title(step, section, term, title):
-    Link(section, term, title).wait_for()
+    TermLink(term, section, title).wait_for()
